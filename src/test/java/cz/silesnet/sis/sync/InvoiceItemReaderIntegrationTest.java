4c801f136d7fafebe348025320cc87c816bd2e4f
der3k <der3k@226666eb-e222-4c08-926c-c284fd0fffcf>
2008-12-27 01:42:54 +0000

    implement Invoice class, implemented SisIvoiceItemReader step

--- src/test/java/cz/silesnet/sis/sync/InvoiceItemReaderIntegrationTest.java
+++ src/test/java/cz/silesnet/sis/sync/InvoiceItemReaderIntegrationTest.java
@@ -1,6 +1,59 @@
 package cz.silesnet.sis.sync;
 
+import javax.sql.DataSource;
 
-public class InvoiceItemReaderIntegrationTest {
+import org.dbunit.IDatabaseTester;
+import org.junit.Test;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.database.DrivingQueryItemReader;
+import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 
+import cz.silesnet.sis.sync.domain.Invoice;
+
+public class InvoiceItemReaderIntegrationTest extends AbstractDependencyInjectionSpringContextTests {
+
+    private IDatabaseTester dbTester;
+    private DataSource dataSource;
+    private DrivingQueryItemReader itemReader;
+    private ExecutionContext executionContext;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void setInvoiceItemReader(DrivingQueryItemReader invoiceItemReader) {
+        this.itemReader = invoiceItemReader;
+    }
+
+    @Override
+    protected String[] getConfigLocations() {
+        return new String[] { "classpath:jobs/sisInvoiceJob.xml" };
+    }
+
+    @Override
+    protected void onSetUp() throws Exception {
+        super.onSetUp();
+        dbTester = SisInvoiceFunctionalTest.initializeDatabase(dataSource);
+        executionContext = new ExecutionContext();
+        itemReader.open(executionContext);
+    }
+
+    @Override
+    protected void onTearDown() throws Exception {
+        super.onTearDown();
+        itemReader.close(executionContext);
+        dbTester.onTearDown();
+    }
+
+    @Test
+    public void testReader() {
+        assertEquals(3, readInvoice().getId());
+        assertEquals(4, readInvoice().getId());
+        assertEquals(5, readInvoice().getId());
+        assertNull(readInvoice());
+    }
+
+    private Invoice readInvoice() {
+        return (Invoice) itemReader.read();
+    }
 }
