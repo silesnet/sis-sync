91b31b3ee8fdf0562d747ee03665f0951e9c540c
der3k <der3k@226666eb-e222-4c08-926c-c284fd0fffcf>
2009-01-04 19:45:25 +0000

    Customer database mapping details done

--- src/main/java/cz/silesnet/sis/sync/mapping/CustomerRowMapper.java
+++ src/main/java/cz/silesnet/sis/sync/mapping/CustomerRowMapper.java
@@ -19,16 +19,34 @@ import cz.silesnet.sis.sync.domain.Customer;
 public class CustomerRowMapper implements RowMapper {
 
     public static final String ID_COLUMN = "id";
-    public static final String NAME_COLUMN = "name";
     public static final String SYMBOL_COLUMN = "symbol";
+    public static final String NAME_COLUMN = "name";
+    public static final String SUPPLEMENTARY_NAME_COLUMN = "supplementary_name";
+    public static final String CONTACT_NAME_COLUMN = "contact_name";
     public static final String CITY_COLUMN = "city";
+    public static final String STREET_COLUMN = "street";
+    public static final String ZIP_COLUMN = "postal_code";
+    public static final String ICO_COLUMN = "public_id";
+    public static final String DIC_COLUMN = "dic";
+    public static final String PHONE_COLUMN = "phone";
+    public static final String EMAIL_COLUMN = "email";
+    public static final String CONTRACT_COLUMN = "contract_no";
 
     public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
         Customer customer = new Customer();
         customer.setId(rs.getLong(ID_COLUMN));
-        customer.setName(rs.getString(NAME_COLUMN));
         customer.setSymbol(rs.getString(SYMBOL_COLUMN));
+        customer.setName(rs.getString(NAME_COLUMN));
+        customer.setSupplementaryName(rs.getString(SUPPLEMENTARY_NAME_COLUMN));
+        customer.setContactName(rs.getString(CONTACT_NAME_COLUMN));
         customer.setCity(rs.getString(CITY_COLUMN));
+        customer.setStreet(rs.getString(STREET_COLUMN));
+        customer.setZip(rs.getString(ZIP_COLUMN));
+        customer.setIco(rs.getString(ICO_COLUMN));
+        customer.setDic(rs.getString(DIC_COLUMN));
+        customer.setPhone(rs.getString(PHONE_COLUMN));
+        customer.setEmail(rs.getString(EMAIL_COLUMN));
+        customer.setContract(rs.getString(CONTRACT_COLUMN));
         return customer;
     }
 
--- src/test/java/cz/silesnet/sis/sync/mapping/CustomerRowMapperTest.java
+++ src/test/java/cz/silesnet/sis/sync/mapping/CustomerRowMapperTest.java
@@ -1,7 +1,11 @@
 package cz.silesnet.sis.sync.mapping;
 
-import static org.junit.Assert.*;
-import static org.easymock.EasyMock.*;
+import static cz.silesnet.sis.sync.mapping.CustomerRowMapper.*;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,11 +19,21 @@ import cz.silesnet.sis.sync.domain.Customer;
 
 public class CustomerRowMapperTest {
 
-    private static final String CUSTOMER_NAME = "Test Customer";
     private static final int IGNORED_ROW_NUM = 0;
-    private static final String CITY = "Test City";
-    private static final String SYMBOL = "12345678";
     private static final long ID = 1234L;
+    private static final String SYMBOL = "12345678";
+    private static final String NAME = "Test Customer";
+    private static final String SUPPLEMENTARY_NAME = "Supplementary Name";
+    private static final String CONTACT_NAME = "Contact Name";
+    private static final String CITY = "Test City";
+    private static final String STREET = "Street 777";
+    private static final String ZIP = "12345";
+    private static final String ICO = "12345678";
+    private static final String DIC = "CZ12345678";
+    private static final String PHONE = "+420123456789";
+    private static final String EMAIL = "contact@customer.cz";
+    private static final String CONTRACT = "2008/0007";
+
     private RowMapper mapper;
     private ResultSet rs;
 
@@ -37,16 +51,34 @@ public class CustomerRowMapperTest {
 
     @Test
     public void testMapRow() throws SQLException {
-        expect(rs.getLong(CustomerRowMapper.ID_COLUMN)).andReturn(ID);
-        expect(rs.getString(CustomerRowMapper.NAME_COLUMN)).andReturn(CUSTOMER_NAME);
-        expect(rs.getString(CustomerRowMapper.SYMBOL_COLUMN)).andReturn(SYMBOL);
-        expect(rs.getString(CustomerRowMapper.CITY_COLUMN)).andReturn(CITY);
+        expect(rs.getLong(ID_COLUMN)).andReturn(ID);
+        expect(rs.getString(SYMBOL_COLUMN)).andReturn(SYMBOL);
+        expect(rs.getString(NAME_COLUMN)).andReturn(NAME);
+        expect(rs.getString(SUPPLEMENTARY_NAME_COLUMN)).andReturn(SUPPLEMENTARY_NAME);
+        expect(rs.getString(CONTACT_NAME_COLUMN)).andReturn(CONTACT_NAME);
+        expect(rs.getString(CITY_COLUMN)).andReturn(CITY);
+        expect(rs.getString(STREET_COLUMN)).andReturn(STREET);
+        expect(rs.getString(ZIP_COLUMN)).andReturn(ZIP);
+        expect(rs.getString(ICO_COLUMN)).andReturn(ICO);
+        expect(rs.getString(DIC_COLUMN)).andReturn(DIC);
+        expect(rs.getString(PHONE_COLUMN)).andReturn(PHONE);
+        expect(rs.getString(EMAIL_COLUMN)).andReturn(EMAIL);
+        expect(rs.getString(CONTRACT_COLUMN)).andReturn(CONTRACT);
         replay(rs);
         Customer customer = (Customer) mapper.mapRow(rs, IGNORED_ROW_NUM);
         assertEquals(ID, customer.getId());
-        assertEquals(CUSTOMER_NAME, customer.getName());
         assertEquals(SYMBOL, customer.getSymbol());
+        assertEquals(NAME, customer.getName());
+        assertEquals(SUPPLEMENTARY_NAME, customer.getSupplementaryName());
+        assertEquals(CONTACT_NAME, customer.getContactName());
         assertEquals(CITY, customer.getCity());
+        assertEquals(STREET, customer.getStreet());
+        assertEquals(ZIP, customer.getZip());
+        assertEquals(ICO, customer.getIco());
+        assertEquals(DIC, customer.getDic());
+        assertEquals(PHONE, customer.getPhone());
+        assertEquals(EMAIL, customer.getEmail());
+        assertEquals(CONTRACT, customer.getContract());
         verify(rs);
     }
 }
