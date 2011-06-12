19a9a18245b9cb2ef1f77612171a1ddeafb18364
der3k <rs.pipe@gmail.com>
2011-06-12 10:23:07 +0200

    migrated log4j configuration to logback

---
+++ src/test/java/cz/silesnet/sis/sync/LoggingTest.java
@@ -0,0 +1,18 @@
+package cz.silesnet.sis.sync;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * User: der3k
+ * Date: 9.6.11
+ * Time: 23:01
+ */
+public class LoggingTest {
+  @Test
+  public void configuration() throws Exception {
+    Logger logger = LoggerFactory.getLogger(LoggingTest.class);
+    logger.debug("Hello {}!", "world");
+  }
+}
